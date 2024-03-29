216276811efd117585b3e15f39cf632bded018c2
Kira Mishima <kiramishima@outlook.com>
2024-03-28 21:05:42 -0600

    updated terraform variables for easy reproduce

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -25,13 +25,13 @@ variable "location" {
 variable "bq_dataset_name" {
   description = "BigQuery Name"
   #Update the below to what you want your dataset to be called
-  default = "bq_crimes_mexico_city"
+  default = "bq_victims_cdmx"
 }
 
 variable "gcs_bucket_name" {
   description = "Storage Bucket Name"
   #Update the below to a unique bucket name
-  default = "crimes-mexico-city-bucket"
+  default = "victims-cdmx-bucket"
 }
 
 variable "gcs_storage_class" {
